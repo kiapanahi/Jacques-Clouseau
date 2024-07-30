78ddc1c1bdcd2d1cca7e0085cd3c80ce36806a88
Kia Raad <kia.panahirad@gmail.com>
2024-07-31 02:00:47 +0330

    Add summary sections to TrackingAdtivity class

--- src/api/Tracking/TrackingActivity.cs
+++ src/api/Tracking/TrackingActivity.cs
@@ -2,11 +2,23 @@
 
 namespace Clouseau.Api.Tracking;
 
+/// <summary>
+/// Represents the tracking activity in the Clouseau API.
+/// </summary>
 internal static class TrackingActivity
 {
+    /// <summary>
+    /// The name of the tracking activity source.
+    /// </summary>
     internal const string TrackingActivitySourceName = "clouseau.tracking.activity";
 
+    /// <summary>
+    /// The version of the tracking activity.
+    /// </summary>
     internal static readonly string TrackingActivityVersion = ServiceDefaults.Diagnostics.SystemVersion;
 
+    /// <summary>
+    /// The activity source for the tracking activity.
+    /// </summary>
     internal static readonly ActivitySource TrackingActivitySource = new(TrackingActivitySourceName, TrackingActivityVersion);
 }
