e0d676922f0fb5578b87a4f4ed5e082b6c5b3597
Kia Raad <kia.panahirad@gmail.com>
2024-07-30 17:28:43 +0330

    chore: Add start-vscode.cmd file to repository

---
+++ start-vscode.cmd
@@ -0,0 +1,6 @@
+@ECHO OFF
+SETLOCAL
+
+:: This command launches a Visual Studio solution with environment variables required to use a local version of the .NET Core SDK.
+
+code "clouseau.code-workspace"
